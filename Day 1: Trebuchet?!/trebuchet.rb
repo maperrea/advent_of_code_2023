6aa43ac554817ff9a20b9fae19548bcd6f2d4fd0
suske <suske@s19.be>
2023-12-01 11:52:34 +0100

    cleaner, tue one liner

--- Day 1: Trebuchet?!/trebuchet.rb
+++ Day 1: Trebuchet?!/trebuchet.rb
@@ -1,5 +1,4 @@
 ### Part 1 ###
-puts File.read("input.txt").split("\n").sum  { |l| (l.chars.find(0) { |c| c.match(/[0-9]/) }).to_i * 10 + (l.chars.reverse_each.find(0) { |c| c.match(/[0-9]/) }).to_i }
+puts File.read("input.txt").split("\n").sum  { |l| {10 => l.chars, 1 => l.chars.reverse_each}.sum { |k, l| l.find(0) { |c| c.match(/[0-9]/) } .to_i * k } }
 ### Part 2 ###
-numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-puts File.read("input.txt").split("\n").sum { |l| (0...l.length).lazy.map { |i| l[i].to_i + numbers.find_index { |n| l[i..].start_with? n }.to_i } .find(0) { |i| i != 0 } * 10 + (0...l.length).reverse_each.lazy.map { |i| l[i].to_i + numbers.find_index { |n| l[i..].start_with? n }.to_i } .find(0) { |i| i != 0 } }
+puts File.read("input.txt").split("\n").sum { |l| {10 => (0...l.length), 1 => (0...l.length).reverse_each}.sum { |k, v| v.lazy.map { |i| l[i].to_i + ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].find_index { |n| l[i..].start_with? n }.to_i } .find(0) { |i| i != 0 } * k } }
