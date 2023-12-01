7625db55eb392af5022e9f05f72b5331cf4c5994
suske <suske@s19.be>
2023-12-01 11:44:35 +0100

    Part 2

--- Day 1: Trebuchet?!/trebuchet.rb
+++ Day 1: Trebuchet?!/trebuchet.rb
@@ -1,3 +1,5 @@
 ### Part 1 ###
 puts File.read("input.txt").split("\n").sum  { |l| (l.chars.find(0) { |c| c.match(/[0-9]/) }).to_i * 10 + (l.chars.reverse_each.find(0) { |c| c.match(/[0-9]/) }).to_i }
 ### Part 2 ###
+numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+puts File.read("input.txt").split("\n").sum { |l| (0...l.length).lazy.map { |i| l[i].to_i + numbers.find_index { |n| l[i..].start_with? n }.to_i } .find(0) { |i| i != 0 } * 10 + (0...l.length).reverse_each.lazy.map { |i| l[i].to_i + numbers.find_index { |n| l[i..].start_with? n }.to_i } .find(0) { |i| i != 0 } }
