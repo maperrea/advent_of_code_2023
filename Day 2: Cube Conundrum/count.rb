4f11cb04c11044e8d1e480aeb8e7750112257a6c
Perreaux Matthis <matperthis@gmail.com>
2023-12-03 01:13:08 +0100

    part 2 one liner

--- Day 2: Cube Conundrum/count.rb
+++ Day 2: Cube Conundrum/count.rb
@@ -1,7 +1,4 @@
-p File.read('input.txt').split("\n").map { |l| l.split(': ') } .sum { |game, input| input.split("; ").all? { |hand| hand.split(", ").map { |balls| [balls.split(' ')[0].to_i, balls.split(' ')[1]] }.all? { |num, col| (col == 'red' && num <= 12) || (col == 'green' && num <= 13) || (col == 'blue' && num <= 14) } } ? game[5..].to_i : 0 }
+### Part 1 ###
+puts File.read('input.txt').split("\n").map { |l| l.split(': ') } .sum { |game, input| input.split("; ").all? { |hand| hand.split(", ").map { |balls| [balls.split(' ')[0].to_i, balls.split(' ')[1]] }.all? { |num, col| (col == 'red' && num <= 12) || (col == 'green' && num <= 13) || (col == 'blue' && num <= 14) } } ? game[5..].to_i : 0 }
 ### Part 2 ###
-puts File.read('input.txt').split("\n").map { |l| l.split(': ') } .sum { |game, input| 
-	maxs = {'red' => 0, 'green' => 0, 'blue' => 0}
-	oui = input.split("; ").each { |hand| hand.split(", ").map { |balls| [balls.split(' ')[0].to_i, balls.split(' ')[1]] }.each { |num, col| maxs[col] = [num, maxs[col]].max } }
-	maxs['red'] * maxs['green'] * maxs['blue']
-}
+puts File.read('input.txt').split("\n").map { |l| l.split(': ') } .sum { |game, input| input.split("; ").each_with_object(Hash.new(0)) { |hand, h| hand.split(", ").map { |balls| [balls.split(' ')[0].to_i, balls.split(' ')[1]] }.each_with_object(h) { |(num, col), h| h[col] = [num, h[col]].max } } .inject(1) { |memo, (col, num)| memo * num } }
